4467ff2cb34e44ccf2e04c3ca363a5329eeb84bb
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-05-26 12:17:24 +0300

    +

--- api/api.ts
+++ api/api.ts
@@ -2,7 +2,7 @@ namespace $ {
 
 	export const $bog_lk_api_countries_endpoint = 'https://countries.trevorblades.com'
 
-	export const $bog_lk_api_petstore_endpoint = process.env?.NODE_ENV === 'prod'
+	export const $bog_lk_api_petstore_endpoint = 'prod' === 'prod'
 		? 'https://petstore3.swagger.io/api/v3'
 		: 'https://STAGE.swagger.io/api/v3'
 
